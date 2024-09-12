b9ddab2da9724109fe8b48ed318f68729e5d0d2e
Cheese <52149451+cheesestudio@users.noreply.github.com>
2024-09-12 17:40:15 +0800

    save and load system

--- Cheese/TableHook/TableHook.cs
+++ Cheese/TableHook/TableHook.cs
@@ -15,15 +15,20 @@ public class TableHook : UdonSharpBehaviour
     [HideInInspector] public float TableColor;
     [HideInInspector] public float TableColorLightness;
 
+    // Cue Skin & Ball Skin
     [HideInInspector] public int inOwner;
-    [HideInInspector]public int outCanUse;
-    private int outCanUseTmp = 0;
-    [HideInInspector] public int ball;
-    public int DefaultCue;
+    [HideInInspector] public byte outCanUse;
+    private byte outCanUseTmp = 0;
+    [HideInInspector] public byte ball;
+    public byte DefaultCue;
     [SerializeField] public bool keepCueRotating;
     private int isRotating;
     [NonSerialized] private int maxRotation=130;
     private Renderer renderer;
+
+    //Save & Load
+    public InputField inputField;
+
     void Start()
     {
         outCanUse = 0;
@@ -31,6 +36,10 @@ public class TableHook : UdonSharpBehaviour
         outCanUseTmp = DefaultCue;
         isRotating = maxRotation;
         renderer = this.transform.Find("body/render").GetComponent<Renderer>();
+
+        //Load PlayerSettings from my server
+        //string LocalData = EncodeLocalData();
+
     }
 
     public void _CanUseCueSkin()
@@ -69,6 +78,141 @@ public class TableHook : UdonSharpBehaviour
     {
         return (float)TableColorLightnessSlider.GetProgramVariable("localValue");
     }
+
+    //Sava and load system
+    #region ConvertFunction
+    private void floatToBytes(byte[] data, int pos, float v)
+    {
+        byte[] bytes = BitConverter.GetBytes(v);
+        Array.Copy(bytes, 0, data, pos, 4);
+    }
+
+    public float bytesToFloat(byte[] data, int pos)
+    {
+        byte[] floatBytes = new byte[4];
+        Array.Copy(data, pos, floatBytes, 0, 4);
+        return BitConverter.ToSingle(floatBytes, 0);
+    }
+
+    private bool isInvalidBase64Char(char value)
+    {
+        var intValue = (int)value;
+
+        // 1 - 9
+        if (intValue >= 48 && intValue <= 57)
+            return false;
+
+        // A - Z
+        if (intValue >= 65 && intValue <= 90)
+            return false;
+
+        // a - z
+        if (intValue >= 97 && intValue <= 122)
+            return false;
+
+        // + or /
+        return intValue != 43 && intValue != 47;
+    }
+
+    private bool isValidBase64(string value)
+    {
+        if (value == null || value.Length == 0 || value.Length % 4 != 0
+            || value.Contains(" ") || value.Contains("\t") || value.Contains("\r") || value.Contains("\n"))
+            return false;
+        var index = value.Length - 1;
+
+        if (value[index] == '=')
+            index--;
+
+        if (value[index] == '=')
+            index--;
+
+        for (var i = 0; i <= index; i++)
+            if (isInvalidBase64Char(value[i]))
+                return false;
+
+        return true;
+    }
+    #endregion
+
+    #region Save & Load
+    // I Call it : Cheese Version ,for short CV,rewrite from "NetworingManagers" 
+    uint LocalDataLength = 11;
+    private string EncodeLocalData()
+    {
+        byte[] gameState = new byte[LocalDataLength];
+        int encodePos = 0;
+        gameState[encodePos] = outCanUseTmp;
+        encodePos += 1;
+        gameState[encodePos] = ball;
+        encodePos += 1;
+        floatToBytes(gameState, encodePos,TableColor);
+        encodePos += 4;
+        floatToBytes(gameState, encodePos, TableColorLightness);
+        encodePos += 4;
+
+        // find gameStateLength
+        //Debug.Log("gameStateLength = " + (encodePos + 1));
+        
+        return "CV:"+Convert.ToBase64String(gameState);
+
+        //Debug.Log("CV:" + Convert.ToBase64String(gameState));
+    }
+
+    private void LoadLocalData(string gameStateStr)
+    {
+        if (!isValidBase64(gameStateStr)) return;
+
+        byte[] gameState = Convert.FromBase64String(gameStateStr);
+        if (gameState.Length != LocalDataLength) return;
+
+        int encoodePos = 0;
+
+        outCanUseTmp = gameState[encoodePos];
+        encoodePos += 1;
+        ball = gameState[encoodePos];
+        encoodePos += 1;
+        TableColor = bytesToFloat(gameState, encoodePos);
+        encoodePos += 4;
+        TableColorLightness = bytesToFloat(gameState,encoodePos);
+        encoodePos += 4;
+
+        ChangeMaterial();
+    }
+
+    public void OnSaveButtonPushed()
+    {
+
+        if (ReferenceEquals(null, inputField))
+        {
+            Debug.Log("Table Hook::OnSaveButtonPushed() inputField property is not set !");
+            return;
+        }
+
+        inputField.text = EncodeLocalData();
+    }
+
+    public void OnLoadButtonPushed()
+    {
+
+        if (ReferenceEquals(null, inputField))
+        {
+            Debug.Log("Table Hook::OnSaveButtonPushed() inputField property is not set !");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(inputField.text))
+        {
+            return;
+        }
+
+
+        LoadLocalData(inputField.text);
+
+    }
+    #endregion
+
+    #region Cue & Ball
     public void _Cue0()
     {
         outCanUseTmp = 0;
@@ -162,4 +306,5 @@ public class TableHook : UdonSharpBehaviour
     {
         ball = 6;
     }
+    #endregion
 }
